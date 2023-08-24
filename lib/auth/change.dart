272f24f4de15d2363110b27890a53ef1009f9d57
latestbob <edidiongbobson@gmail.com>
2023-08-24 11:20:59 -0700

    added changed file

---
+++ lib/auth/change.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChangeWidgt extends StatefulWidget {
+  const ChangeWidgt({super.key});
+
+  @override
+  State<ChangeWidgt> createState() => _ChangeWidgtState();
+}
+
+class _ChangeWidgtState extends State<ChangeWidgt> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
